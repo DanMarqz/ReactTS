042348a5dd5ee7a3b665b75b428b4ddc4b1a7011
Daniel <danielarturo.marquez@gmail.com>
2021-01-26 13:04:30 -0400

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+// useState
 import { Counter } from './components/Counter';
 import { Usuario } from './components/Usuario';
+// useEffect
+import { TimerPadre } from './components/TimerPadre';
 
 function App() {
   return (
@@ -7,11 +10,14 @@ function App() {
       <h1>React + TypeScript</h1>
       <hr className="mt-4"/>
 
+      <h2>useState</h2>
       <Counter />
 
+      <Usuario />
       <hr className="mt-5"/>
 
-      <Usuario />
+      <h2>useEffect - useRef</h2>
+      <TimerPadre />
 
     </>
   );
